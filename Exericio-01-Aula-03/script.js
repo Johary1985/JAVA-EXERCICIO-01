5ebff23713b5905bc227797752cc8ba9efe55332
Johary1985 <77841530+Johary1985@users.noreply.github.com>
2021-02-13 19:38:59 -0300

    Add files via upload

---
+++ Exericio-01-Aula-03/script.js
@@ -0,0 +1,11 @@
+let Usuario = prompt ("Usuario")
+let Turma = prompt ("Turma")
+let Modulo = prompt ("Modulo")
+
+var Table = ([
+    "Usuario",
+    "Turma",
+    "Modulo",
+])
+
+console.table([Table, Usuario, Turma, Modulo]);
